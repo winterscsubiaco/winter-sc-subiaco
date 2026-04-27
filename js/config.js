8eb1a0f7964524dad8a01e962f0a6533e338c9d3
winterscsubiaco <wintersc.subiaco@gmail.com>
2026-04-28 00:02:47 +0200

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@
 // ============================================================
 
 const SUPABASE_URL = 'https://mvxwwpqbgthlzwympokg.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_VB_J-UOJq9uJR_9OBeHXAQ_CTqoPQqj';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im12eHd3cHFiZ3RobHp3eW1wb2tnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzczMTY0NTksImV4cCI6MjA5Mjg5MjQ1OX0.yibZM9kfdYuMWIUS2HqgZQV5uzvZulUbTYRI2fVgk4I';
 
 const db = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
